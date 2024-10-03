50e6bfccace1ce30625b054529df48035054b8d2
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-10-03 17:03:17 +0700

    hero component :sparkles:

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Header } from "@/components";
+import { Header, Hero } from "@/components";
 
 export default function Home() {
 	return (
 		<div className="bg-white min-h-screen">
 			<Header />
+			<Hero />
 		</div>
 	);
 }
