4c9d6902037144f355f014b413528b488f5bb959
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-30 07:22:56 +0700

    component:sparkles: - stack component

--- src/components/ui/Stack/Stack.tsx
+++ src/components/ui/Stack/Stack.tsx
@@ -2,27 +2,47 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./Stack.module.css";
 import { ResponsiveMap } from "@/types";
+import { generateResponsiveStyles } from "@/utils";
+
+const StackFlex = ["initial", "1 1 0%", "1 1 auto", "none"] as const;
+const StackDirection = ["column", "row"] as const;
+const StackAlign = ["stretch", "center", "start", "end", "baseline"] as const;
+const StackJustify = ["normal", "flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly", "stretch"] as const;
 
 export type StackType = {
-	flex?: string;
-	direction?: string;
-	align?: string;
-	justify?: string;
+	flex?: string | ResponsiveMap<string>;
+	direction?: string | ResponsiveMap<string>;
+	align?: string | ResponsiveMap<string>;
+	justify?: string | ResponsiveMap<string>;
 	padding?: string | ResponsiveMap<string>;
 	gap?: string | ResponsiveMap<string>;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export const Stack: React.FC<StackType> = ({ children }) => {
+export const Stack: React.FC<StackType> = ({ 
+	flex = StackFlex[0], 
+	direction = StackDirection[0], 
+	align = StackAlign[0], 
+	justify = StackJustify[1], 
+	padding = "0", 
+	gap = "0", 
+	children,
+	className,
+	style, 
+}) => {
 	const stackStyles = {
-		"--stack-flex": "initial",
-		"--stack-direction": "column",
-		"--stack-align": "stretch",
-		"--stack-justify": "flex-start",
-		"--stack-padding": "0px",
-		"--stack-gap": "12px",
+		...generateResponsiveStyles("flex", flex),
+		...generateResponsiveStyles("direction", direction),
+		...generateResponsiveStyles("align", align),
+		...generateResponsiveStyles("justify", justify),
+		...generateResponsiveStyles("padding", padding),
+		...generateResponsiveStyles("gap", gap),
 	} as React.CSSProperties;
+
 	return (
-		<div className={clsx(styles.stack)} style={{ ...stackStyles }}>
+		<div
+			className={clsx(styles.stack, className)}
+			style={{...style, ...stackStyles }}
+		>
 			{children}
 		</div>
 	);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,10 +3,22 @@ import React from "react";
 import { ResponsiveMap } from "@/types";
 
 // Utility function to extract responsive values
-export const getResponsiveValue = <T>(
-	value: T | ResponsiveMap<T>,
-	prop: "xs" | "sm" | "smd" | "md" | "lg" | "xl"
-): T | undefined => {
+export const getResponsiveValue = <T>(value: T | ResponsiveMap<T>, prop: "xs" | "sm" | "smd" | "md" | "lg" | "xl"): T | undefined => {
 	if (typeof value !== "object" || value === null) return undefined;
 	return (value as ResponsiveMap<T>)[prop];
+};
+
+export const generateResponsiveStyles = (property: string, value: string | ResponsiveMap<string>) => {
+	const breakpoints: Array<keyof ResponsiveMap<string>> = ["xs", "sm", "smd", "md", "lg", "xl"];
+	const styles: { [key: string]: string | undefined } = {};
+
+	if (typeof value === "object") {
+		breakpoints.forEach((bp) => {
+			styles[`--${bp}-stack-${property}`] = value[bp];
+		});
+	} else {
+		styles[`--stack-${property}`] = value;
+	}
+
+	return styles;
 };
