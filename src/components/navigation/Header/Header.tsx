01c67f923db0ea80937cc12090e608c50c6d530b
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-12-01 14:16:43 +0700

    feat - cta header

--- src/components/navigation/Header/Header.tsx
+++ src/components/navigation/Header/Header.tsx
@@ -2,10 +2,19 @@
 import clsx from "clsx";
 import styles from "./Header.module.css";
 import React from "react";
-import { Link, NavigationMenu } from "@/components";
+import { Link, NavigationMenu, Stack } from "@/components";
 
 export const Header = () => {
 	const [hasScrolled, setHasScrolled] = React.useState<boolean>(false);
+	const [mounted, setMounted] = React.useState<boolean>(false);
+
+	React.useEffect(() => {
+		setMounted(true);
+
+		return () => {
+			setMounted(false);
+		};
+	}, []);
 
 	React.useEffect(() => {
 		const handleScroll = () => {
@@ -43,6 +52,23 @@ export const Header = () => {
 							/>
 						</div>
 					</div>
+
+					<Stack 
+						direction="row" 
+						align="center"
+						gap="12px"
+						className={clsx(
+							styles['right-section_content'],
+							mounted && styles['right-section_loaded']
+						)}
+					>
+						<Stack
+							className={clsx(styles['right-section_nonPrimaryContent'])}
+							style={{ opacity: 1, filter: "blur(0px)" }}
+						>
+
+						</Stack>
+					</Stack>
 				</header>
 		</div>
 	);
